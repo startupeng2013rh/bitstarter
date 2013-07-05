107bdaab2705219efc1251d2b6c1ac0bbd7ef992
startupeng2013rh <startupeng2013rh@gmail.com>
2013-07-05 15:24:58 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer('Hello World from', 'utf-8');
+
+//buffer.toString('utf8') +
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html', 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
